3407e4114e4896ec1e4b9534ed44b4f2dfe5eb29
Gaddam Anjali <anjali.gaddam@everest.engineering>
2021-03-16 11:02:43 +0530

    Practice arrays in ruby

---
+++ 6.arrays/array_operations.rb
@@ -0,0 +1,32 @@
+array = [1, 'Bob', 4.33, 'another string']
+
+puts array.first
+puts array.first(2)
+puts array.last
+puts array[3]
+
+array.push(3)
+puts array
+array.pop
+puts array
+
+array.concat([2, 3])
+array << 3
+puts array
+
+array = [1, 2, 3, 4]
+array.map{|num| num**2}
+array
+array.collect{|num| num**2}
+array
+array.delete_at(2)
+array
+array.delete(3)
+array
+array.uniq
+array.select {|num| num>4}
+array.include?(3)
+array.each_index { |i| puts "This is index #{i}" }
+array.each_with_index { |val, idx| puts "#{idx+1}. #{val}" }
+puts array.sort
+puts array
---
+++ 6.arrays/play_with_arrays.rb
@@ -0,0 +1,15 @@
+arr = ["b", "a"]
+arr = arr.product(Array(1..3))
+print arr
+arr.first.delete(arr.first.last)
+puts
+print arr
+
+puts
+puts
+arr = ["b", "a"]
+arr = arr.product([Array(1..3)])
+print arr
+arr.first.delete(arr.first.last)
+puts
+print arr
